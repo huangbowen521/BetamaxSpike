aa8ce2d321b350c7a16f813d015eb6ddb7bed499
cuipengfei <cuipengfei2008@gmail.com>
2012-10-19 16:57:58 +0800

    introduce super class for services that handles vcr

---
+++ src/main/java/com/thoughtworks/Service.java
@@ -0,0 +1,11 @@
+package com.thoughtworks;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: twer
+ * Date: 10/19/12
+ * Time: 2:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Service {
+}
--- src/test/java/com/thoughtworks/WeatherServiceNGTest.java
+++ src/test/java/com/thoughtworks/WeatherServiceNGTest.java
@@ -4,17 +4,20 @@ import co.freeside.betamax.Betamax;
 import co.freeside.betamax.TapeMode;
 import com.thoughtworks.extension.NGRecorder;
 import com.thoughtworks.extension.NGRule;
+import com.thoughtworks.extension.RulesListener;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-public class WeatherNGTest {
+@Listeners(RulesListener.class)
+public class WeatherServiceNGTest {
     @NGRule
     public NGRecorder recorder = new NGRecorder();
 
-    @Betamax(tape = "my tape", mode = TapeMode.READ_WRITE)
+    @Betamax(tape = "WeatherService", mode = TapeMode.READ_WRITE)
     @Test
     public void shouldGetWeatherViaWebService2() throws Exception {
         WeatherService weatherService = new WeatherService();
-        String result = weatherService.getWeather("Washington");
+        String result = weatherService.sendRequest("Washington");
         org.testng.Assert.assertEquals(result.isEmpty(), false);
     }
 }
