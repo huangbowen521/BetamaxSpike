60e01db9cdc37d91ca8245d2f6408d29862ea907
Bowen <huangbowen521@gmail.com>
2012-10-19 11:16:33 +0800

    implement to use betamax at development.

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -1,7 +1,13 @@
 package com.thoughtworks;
 
+import co.freeside.betamax.MatchRule;
+import co.freeside.betamax.Recorder;
+import co.freeside.betamax.TapeMode;
+import groovy.lang.Closure;
+
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,13 +19,19 @@ import java.net.MalformedURLException;
 public class App {
 
     public static void main(String[] args) {
-        try {
-            Weather weather = new Weather();
-            weather.getWeather("Washington");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        final Weather weather = new Weather();
+
+        Recorder recorder = new Recorder();
+        Map<Object,Object> map = new HashMap<Object, Object>();
+        map.put("mode",TapeMode.DEFAULT);
+        map.put("match", new MatchRule[]{MatchRule.method,MatchRule.uri});
+
+        recorder.withTape("my tape", map, new Closure(null) {
+           public void doCall() throws IOException {
+              weather.getWeather("Chicago");
+           }
+        });
     }
+
+
 }
