2c90807f02e82f463d8f4724ae8222b27180a75d
cuipengfei <cuipengfei2008@gmail.com>
2012-10-24 09:49:19 +0800

    remove old files

--- src/test/resources/BetamaxConfig.groovy
+++
@@ -1,9 +0,0 @@
-betamax {
-    tapeRoot = new File('src/test/resources/betamax/tapes')
-    proxyPort = 1337
-    proxyTimeout = 30000
-    defaultMode = TapeMode.READ_ONLY
-    ignoreHosts = []
-    ignoreLocalhost = true
-    sslSupport = true
-}
